c840450ce588740e33395cd59a8e7e62de64a5b3
eluisking <luistecisc@gmail.com>
2024-07-26 23:07:01 -0600

    [ADD] Nav-Desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors:{
+      'orange-primary': '#ff6e5c',
+      'orange-secundary': '',
+      'bg-primary': '#f1f5f9',
+      'bg-secundary': '#94a3b8',
+      'bg-gray': '#1e293b',
+      'black': '#020617',
+      'white': '#f8fafc',
+    },
+    fontFamily:{
+      'Poppins-r': ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage:{
+        'cover-ds': "url('/public/img/cover-desktop.jpg')"
+      },
+      spacing: {
+        '128': '32rem'
+      }
+    },
   },
   plugins: [],
 }
